7356d33256b82195d136fd0e932912f87833c48e
Sergey Kosarevsky <sk@linderdaum.com>
2013-11-15 14:38:36 +0400

    3_ShadersAndVertexArrays: cleaner source code formatting in clGLSLShaderProgram

--- Chapter6/3_ShadersAndVertexArrays/src/GLSL.h
+++ Chapter6/3_ShadersAndVertexArrays/src/GLSL.h
@@ -32,7 +32,6 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-// Ahtung!  Not portable code detected in Android!!!
 #pragma once
 
 #include "Platform.h"
@@ -45,36 +44,31 @@ struct sUniform
 public:
 	explicit sUniform( const std::string& Name ): FName( Name ), FLocation( -1 ) {};
 	sUniform( int Location, const std::string& Name ): FName( Name ), FLocation( Location ) {};
-	std::string      FName;  // Fancy aligment.  See first time.
-	Lint         FLocation;
+	std::string FName;
+	Lint        FLocation;
 };
 
 class clGLSLShaderProgram
 {
 public:
-	// Again, disallow copy/assign.
 	clGLSLShaderProgram( const std::string& VShader, const std::string& FShader );
-	// This shouldn't be virtual, you have no virtual function to
-	// override.  Managing through base class pointer is useless.
-	virtual ~clGLSLShaderProgram();
+	~clGLSLShaderProgram();
 
-	bool       RelinkShaderProgram();
-	void       Bind();
-	Lint       CreateUniform( const std::string& Name );
-                   // Why |const float|?
-	void       SetUniformNameFloat( const std::string& Name, const float Float );
-	           // Don't use |const float&|.
-	void       SetUniformNameFloatArray( const std::string& Name, int Count, const float& Float );
-	void       SetUniformNameVec3Array( const std::string& Name, int Count, const LVector3& Vector );
-	void       SetUniformNameVec4Array( const std::string& Name, int Count, const LVector4& Vector );
-	void       SetUniformNameMat4Array( const std::string& Name, int Count, const LMatrix4& Matrix );
+	bool RelinkShaderProgram();
+	void Bind();
+	Lint CreateUniform( const std::string& Name );
+	void SetUniformNameFloat( const std::string& Name, const float Float );
+	void SetUniformNameFloatArray( const std::string& Name, int Count, const float& Float );
+	void SetUniformNameVec3Array( const std::string& Name, int Count, const LVector3& Vector );
+	void SetUniformNameVec4Array( const std::string& Name, int Count, const LVector4& Vector );
+	void SetUniformNameMat4Array( const std::string& Name, int Count, const LMatrix4& Matrix );
 
 private:
-	void     BindDefaultLocations( Luint ProgramID );
-	Lint     FindUniformLocation( const std::string& Name );
-	Luint    AttachShaderID( Luint Target, const std::string& ShaderCode, Luint OldShaderID );
-	bool     CheckStatus( Luint ObjectID, Lenum Target, const std::string& Message ) const;
-	void     RebindAllUniforms();
+	void  BindDefaultLocations( Luint ProgramID );
+	Lint  FindUniformLocation( const std::string& Name );
+	Luint AttachShaderID( Luint Target, const std::string& ShaderCode, Luint OldShaderID );
+	bool  CheckStatus( Luint ObjectID, Lenum Target, const std::string& Message ) const;
+	void  RebindAllUniforms();
 private:
 	std::string FVertexShader;
 	std::string FFragmentShader;
