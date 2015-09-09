bff947d508f66824fdf392468e1b32ab06c7e524
Sergey Kosarevsky <sk@linderdaum.com>
2015-09-09 12:45:57 +0200

    Picasa: http -> https

--- Chapter9/Engine/network/Picasa.h
+++ Chapter9/Engine/network/Picasa.h
@@ -37,7 +37,7 @@
 #include <string>
 #include <vector>
 
-const std::string Picasa_ListURL = "http://picasaweb.google.com/data/feed/api/featured/?kind=photo&imgmax=1600&max-results=15";
+const std::string Picasa_ListURL = "https://picasaweb.google.com/data/feed/api/featured/?kind=photo&imgmax=1600&max-results=15";
 
 std::string Picasa_GetDirectImageURL( const std::string& BaseURL, int ImgSizeType );
 void Picasa_ParseXMLResponse( const std::string& Response, std::vector<std::string>& URLs );
